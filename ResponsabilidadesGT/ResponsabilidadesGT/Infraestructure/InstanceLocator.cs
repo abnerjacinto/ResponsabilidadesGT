009d8809a7d92cc7826625a0b13b60ec4ea2abb9
Abner Jacinto <abner.jacinto@gmail.com>
2018-09-05 18:58:35 -0600

    avance

---
+++ ResponsabilidadesGT/ResponsabilidadesGT/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using ResponsabilidadesGT.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsabilidadesGT.Infraestructure
+{
+    class InstanceLocator
+    {
+        #region propierties
+        public MainViewModel Main { get; set; }
+
+        #endregion
+        #region Constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
