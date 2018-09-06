16913057af9afe339f9cac463e986e5444db68fd
Abner Jacinto <abner.jacinto@gmail.com>
2018-09-06 13:14:54 -0600

    Login ViewModel

---
+++ ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ResponsabilidadesGT.ViewModels
+{
+    public class BaseViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs((propertyName)));
+        }
+
+        protected void SetValue<T>(ref T backinFiel, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backinFiel, value))
+            {
+                return;
+            }
+            backinFiel = value;
+            OnPropertyChanged(propertyName);
+
+            
+        }
+    }
+}
--- ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/LoginViewModel.cs
+++ ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/LoginViewModel.cs
@@ -1,10 +1,132 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ResponsabilidadesGT.ViewModels
 {
-    class LoginViewModel
+    class LoginViewModel:INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region attributes
+        private string email { get; set; }
+        private string password { get; set; }
+        private bool isEnabled { get; set; }
+        private bool isRunning { get; set; }
+        #endregion
+        #region Propierties
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                if(this.email!=value)
+                {
+                    this.email = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.Email)));
+                }
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if (this.password != value)
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
+        }
+        public bool IsRemember { get; set; }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
+            }
+        }
+        #endregion
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+            this.IsEnabled = true;
+        }
+        #endregion
+        #region Commad
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe de ingresar un correo",
+                    "Ok");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe de ingresar una contraseña",
+                    "Ok");
+                return;
+            }
+        }
+        #endregion
     }
 }
