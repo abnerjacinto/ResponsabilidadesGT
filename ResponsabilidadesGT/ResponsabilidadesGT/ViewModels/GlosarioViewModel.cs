a8d1cbce40f5913a8fb2e6fa8309433b181840c1
Marco Abner Jacinto <abner.jacinto@gmail.com>
2018-09-18 22:13:56 -0600

    Glosario view

--- ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/GlosarioViewModel.cs
+++ ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/GlosarioViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace ResponsabilidadesGT.ViewModels
+﻿
+namespace ResponsabilidadesGT.ViewModels
 {
+    using Models;
     using System.Collections.Generic;
     class GlosarioViewModel
     {
+        #region Properties
+        public Obligacion Obligacion { get; set; }
+        #endregion
+
+
+        #region Constructor
+        public GlosarioViewModel(Obligacion obligacion)
+        {
+            this.Obligacion = obligacion;
+        } 
+        #endregion
     }
 }
--- ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/ObligacionItemViewModel.cs
+++ ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/ObligacionItemViewModel.cs
@@ -1,8 +1,29 @@
 ﻿
 namespace ResponsabilidadesGT.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using Models;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+    using Views;
+
     public class ObligacionItemViewModel: Obligacion
     {
+        #region Commands
+        public ICommand SelectObligacionCommand
+        {
+            get
+            {
+                return new RelayCommand(SelectObligacion);
+            }
+        }
+
+        private async void SelectObligacion()
+        {
+            MainViewModel.GetInstance().Glosario = new GlosarioViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new GlosarioPage());
+        }
+        #endregion
     }
 }
