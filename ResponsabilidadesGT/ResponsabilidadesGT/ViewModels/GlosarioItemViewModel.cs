87476cd8ece89297bfd32cdc8010c33f5e1700b3
Abner Jacinto <abner.jacinto@gmail.com>
2018-09-24 18:09:37 -0600

    a

--- ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/GlosarioItemViewModel.cs
+++ ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/GlosarioItemViewModel.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace ResponsabilidadesGT.ViewModels
 {
-    class GlosarioItemViewModel
+    using Models;
+    using Xamarin.Forms;
+
+    public class GlosarioItemViewModel:Glosario
     {
+        #region Construct
+        private async void SelectPuntos()
+        {
+            MainViewModel.GetInstance().Glosario = new GlosarioViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new PuntoAtencionPage());
+        }
+        #endregion
     }
 }
--- ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/GlosarioViewModel.cs
+++ ResponsabilidadesGT/ResponsabilidadesGT/ViewModels/GlosarioViewModel.cs
@@ -3,8 +3,11 @@ namespace ResponsabilidadesGT.ViewModels
 {
     using Models;
     using Services;
-    using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using Xamarin.Forms;
+
     public class GlosarioViewModel
     {
         #region Service
@@ -12,8 +15,11 @@ namespace ResponsabilidadesGT.ViewModels
         #endregion
         #region Properties
         public Obligacion Obligacion { get; set; }
+        public Glosario Glosario { get; set; }
+        public ObservableCollection<GlosarioItemViewModel> Glosarios { get; set; }
+         
         #endregion
-        
+
 
         #region Constructor
         public GlosarioViewModel(Obligacion obligacion)
@@ -24,9 +30,50 @@ namespace ResponsabilidadesGT.ViewModels
         }
         #endregion
         #region methods
-        private void LoadGlosario()
+        private async void LoadGlosario()
         {
-            throw new NotImplementedException();
+               
+                var connection = await this.apiservice.CheckConnection();
+                if (!connection.IsSuccess)
+                {
+                        await Application.Current.MainPage.DisplayAlert(
+                        "Error",
+                        connection.Message,
+                        "ok");
+                    await Application.Current.MainPage.Navigation.PopAsync();
+                    return;
+                }
+                var response = await this.apiservice.GetList<Glosario>("http://192.168.10.3",
+                    "/ResponsabilidadesGT",
+                    "/public/getglosario");
+                if (!response.IsSuccess)
+                {
+                    
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Error",
+                        response.Message,
+                        "ok");
+                }
+            
+                MainViewModel.GetInstance().GlosarioList = (List<Glosario>)response.Result;
+                //this.ObligacionesList = (List<Obligacion>)response.Result;
+                this.Glosarios  = new ObservableCollection<GlosarioItemViewModel>(
+                    this.ToItemGlosarioViewModel());
+
+            
+        }
+        private IEnumerable<GlosarioItemViewModel> ToItemGlosarioViewModel()
+        {
+            return MainViewModel.GetInstance().GlosarioList.Select(l => new ObligacionItemViewModel
+            {
+                IdObligacion = l.IdObligacion,
+                NombreObligacion = l.NombreObligacion,
+                EstadoObligacion = l.EstadoObligacion,
+                UsuarioAdicionoObligacion = l.UsuarioAdicionoObligacion,
+                FechaAdicionoObligacion = l.FechaAdicionoObligacion,
+                UsuarioModificoObligacion = l.UsuarioModificoObligacion,
+                FechaModificoObligacion = l.FechaModificoObligacion
+            });
         }
         #endregion
     }
